be85732675b7e07f3d122682dc0ceb700b09b026
James Elliott <jjellio@sandia.gov>
2017-11-20 14:30:29 -0700

    updated constrained yaxis

--- SolverDriver/python/comparison.sh
+++ SolverDriver/python/comparison.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
-CSVDIR=/home/www/datasets/onyx-v-cori
+CSVDIR=${HOME}/perf-data/datasets/onyx-v-cori
 
-BL="onyx-frozen onyx"
+BL="onyx-frozen"
+machine=theta
+expected_baseline_speedup=1.0
+COMPS="default reboot switches"
 
-COMPS="reboot switches"
+for machine in theta cori; do
+if [ "$machine" == "theta" ]; then
+expected_baseline_speedup=1.0;
+else
+expected_baseline_speedup=`echo "scale=10; 1.3/1.4" | bc`
+fi
+
 
 for data in ${BL}; do
   for comp in ${COMPS}; do
-    for kernel in tpetra muelu; do
-      d="${data}_v_cori-${kernel}-${comp}"
+    for kernel in muelu tpetra; do
+      d="${data}_v_${machine}-${kernel}-${comp}"
       mkdir ${d}
       pushd ${d}
       CMD="${HOME}/src/TrilinosDrivers/SolverDriver/python/plot_openmp_weak.py"
       OPTS=""
       read -r -d '' OPTS <<- EOM
            --dataset=${CSVDIR}/${data}-${kernel}.csv
-           --baseline=${CSVDIR}/cori-${kernel}-${comp}.csv
+           --baseline=${CSVDIR}/${machine}-${kernel}-${comp}.csv
+           --expected_baseline_speedup=${expected_baseline_speedup}
            --scaling=weak
            --max_only
            --max_nodes=64
@@ -26,21 +36,25 @@ for data in ${BL}; do
            --annotate_filenames
            --plot=raw_data,bl_speedup
            --plot_titles=ht
+           --number_plots=false
 EOM
 
       if [ "$kernel" == "muelu" ]; then
         OPTS="${OPTS} --study=muelu_constructor --restrict_timer_labels=muelu_levels --sort_timer_labels=maxT"
+        OPTS+=" --average=ns"
       elif [ "$kernel" == "tpetra" ]; then
-        OPTS="${OPTS} --study=linearAlg"
+        OPTS="${OPTS} --study=linearAlg --sort_timer_labels=maxT"
+        OPTS+=" --average=cc"
       fi
-      
+
       echo "$CMD $OPTS"
       ls -1 ${CSVDIR}/${data}-${kernel}.csv
-      ls -1 ${CSVDIR}/cori-${kernel}-${comp}.csv
+      ls -1 ${CSVDIR}/${machine}-${kernel}-${comp}.csv
       echo $CMD $OPTS &> gen.log
-      $CMD $OPTS &> gen.log &
+      $CMD $OPTS >> gen.log 2>&1 &
 
       popd
     done
   done
+done
 done
